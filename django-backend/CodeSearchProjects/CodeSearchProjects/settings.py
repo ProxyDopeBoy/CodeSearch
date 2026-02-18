eb1caeb49994ba67dbb7e67c2f4792c13769e7f6
Proxy <proxorche@gmail.com>
2026-02-18 13:40:51 +0400

    Updating settings py

--- django-backend/CodeSearchProjects/CodeSearchProjects/settings.py
+++ django-backend/CodeSearchProjects/CodeSearchProjects/settings.py
@@ -12,19 +12,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os 
+from dotenv import load_dotenv
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tkpj!#*fx&b(zuj=9sb83m=#v7fl&47m1m5(+-)(l22kpti&6@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+if not SECRET_KEY:
+    raise ValueError("SECRET KEY not found in .env file")
 
 ALLOWED_HOSTS = []
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'CodeSearchApp',
 
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
